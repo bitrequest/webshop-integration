b88bfab51a2bc0abb3d465e6b19d19bdc1f4c5e1
bitrequest <mail@bitrequest.io>
2022-03-22 18:24:29 +0100

    lightning support

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Live demo: [https://www.bitrequest.io/brewery/](https://www.bitrequest.io/brewer
 ### Steps
 #### 1. Add the following resources to your html.
     <link rel="stylesheet" href="https://bitrequest.github.io/assets_styles_lib_bitrequest.css"/>
-    <script src="https://bitrequest.github.io/assets_js_lib_jquery-3.3.1.min.js"></script>
+    <script src="https://bitrequest.github.io/assets_js_lib_jquery-3.6.0.min.js"></script>
     <script src="https://bitrequest.github.io/assets_js_lib_bitrequest_checkout.js"></script>
 
 You can skip the jquery file if you already use jquery on your site.  
@@ -13,21 +13,21 @@ Or you can use your own jquery reference instead.
 #### 2. Generate a request link (example).  
 For assistance visit [https://www.bitrequest.io/request-url/](https://www.bitrequest.io/request-url/)  
 
-    var payment = 'nano';
-    var uoa = 'usd';
+    var payment = "nano";
+    var uoa = "usd";
     var amount = 0.05;
-    var address = 'nano_13kx8gfrse6q6z6djznkhj83cshih8akmjr3e3wmoabb38p6immo4mho8359';
+    var address = "nano_13kx8gfrse6q6z6djznkhj83cshih8akmjr3e3wmoabb38p6immo4mho8359";
     var d = btoa(JSON.stringify({
-        't': 'Example request title',
-        'n': 'Example request name',
-        'c': 0,
-        'pid': 'paymentid'
+        "t": "Example request title",
+        "n": "Example request name",
+        "c": 0,
+        "pid": "paymentid"
     }));
     // optional: set &contactform=true to show contact/shipping form
-    var request_url = 'https://bitrequest.github.io/?payment=' + payment + '&uoa=' + uoa + '&amount=' + amount + '&address=' + address + '&d=' + d;
+    var request_url = "https://bitrequest.github.io/?payment=" + payment + "&uoa=" + uoa + "&amount=" + amount + "&address=" + address + "&d=" + d;
 
 #### 3. Checkout link.  
-Every link with class .br_checkout will trigger the payment terminal popup. Make sure the href attribute has the correctly formatted request link.
+Every link with class .br_checkout will trigger the payment request popup. Make sure the href attribute has the correctly formatted request link.
 
     <a href="{$request_url}" class="br_checkout">Check out</a>
 
@@ -69,5 +69,29 @@ You can catch the postdata with the following callback function and update your 
     function result_callback(post_data) {
         console.log(post_data);
     }
+
+There's an example backend php template included in this repository.
+
+## Lightning requests
+
+For lightning request you will need to setup a [proxy server](https://github.com/bitrequest/bitrequest.github.io/tree/master/proxy) and connect to your lightning node.
+
+Add the following key / values to your data parameter:
+
+    var payment = "bitcoin";
+    var uoa = "usd";
+    var amount = 0.05;
+    var address = "lnurl" (lightning only) or a btc-address (hybrid);
+    var d = btoa(JSON.stringify({
+        "t": "Example request title",
+        "n": "Example request name",
+        "c": 1,
+        "pid": {$random payment-id}} (required),
+        "imp": "lnd" / "eclair" / "c-lightning" / "lnbits" (required),
+	    "proxy": {$proxy host} (url or lnurl) (required),
+	    "pw": {$your proxy api key} (optional)
+    }));
+
+You can set a callback url in the [proxy config file](https://github.com/bitrequest/bitrequest.github.io/tree/master/proxy) to receive updates on your back-end.  
 
 There's an example backend php template included in this repository.
--- webhook/index.php
+++ webhook/index.php
@@ -1,87 +1,30 @@
 <?php
-header("Content-Type: application/json");
-
-// security
 $host = $_SERVER["HTTP_HOST"];
-$origin = $_SERVER["HTTP_ORIGIN"];
-$post_origin = (strpos($origin, $host)) ? "local" : "remote"; // check if post request is local or remote
-if ($post_origin == "local") { // only allow local requests
-    $security_object = array(
-        "host" => $host,
-        "origin" => $origin,
-        "post_origin" => $post_origin
-    );
+$post_data = json_encode(array(
+    "post" => $_POST,
+    "get" => $_GET
+), JSON_PRETTY_PRINT);
 
-    $txdata = $_POST["txdata"];
-    $payment = $txdata["payment"];
-    $ccvalue = $txdata["receivedcc"];
-    $currencyname = $txdata["currencyname"];
-    $fiatvalue = $txdata["fiatvalue"];
-    $txhash = $txdata["txhash"];
-    $iscrypto = $txdata["iscrypto"];
+// response email:
+//$recipient = "recipient@domain.com"; // uncomment and enter your recipient email-address here
+//$sender = "sender@domain.com"; // uncomment and enter your sender email-address here
+$subject = "Post data";
+$message = "<body style='font-family:helvetica,arial,sans-serif;font-size:12px;color:#5d5d5d;padding:20px'>
+		<p style='line-height:1.3em'>
+			<strong>post_data: </strong><br/>
+			<pre>" . $post_data . "</pre><br/>
+			<strong>Callback host: </strong><br/>
+			<pre>" . $host . "</pre>
+		</p>
+	</body>";
+$headers = array(
+    "MIME-Version" => "1.0",
+    "Content-type" => "text/html; charset=UTF-8",
+    "From" => $sender,
+    "Reply-To" => $sender,
+    "X-Mailer" => "PHP/" . phpversion()
+);
+$mailresult = mail($recipient, $subject, $message, $headers);
+return
 
-    $data = $_POST["data"];
-    $meta = $_POST["meta"];
-    $contact = $_POST["contact"];
-
-    // response json:
-    $json_object = array(
-        "txdata" => $txdata,
-        "data" => $data,
-        "meta" => $meta,
-        "contact" => $contact,
-        "security" => $security_object
-    );
-
-    $ccval_string = $ccvalue . " " . $payment;
-    $value_string = ($iscrypto == "true") ? $ccval_string : $ccval_string . " (" . $fiatvalue . " " . $currencyname . ")";
-
-    $post_data = json_encode(array(
-        "result" => $json_object
-    ) , JSON_PRETTY_PRINT);
-
-    // response email:
-    //$recipient = "recipient@domain.com"; // uncomment and enter your recipient email-address here
-    //$sender = "sender@domain.com"; // uncomment and enter your sender email-address here
-    $subject = "You've received a new " . $payment . " payment";
-    $message = "<body style='font-family:helvetica,arial,sans-serif;font-size:12px;color:#5d5d5d;padding:20px'>
-			<p style='line-height:1.3em'>
-				<strong>Amount</strong> " . $value_string . "<br/><br/>
-				<strong>Purchased</strong> " . $_POST["data"]["t"] . "<br/><br/>
-				<strong>post_data: </strong><br/>
-				<pre>" . $post_data . "</pre><br/>
-				<a href='https://bitrequest.github.io/?p=requests&txhash=" . $txhash . "' target='_blank'>View transaction</a>
-			</p>
-		</body>";
-    $headers = array(
-        "MIME-Version" => "1.0",
-        "Content-type" => "text/html; charset=UTF-8",
-        "From" => $sender,
-        "Reply-To" => $sender,
-        "X-Mailer" => "PHP/" . phpversion()
-    );
-    if ($contact !== NULL) {
-        $confirm_subject = "Thank you for your purchase at " . $data["n"];
-        mail($contact["email"], $confirm_subject, $message, $headers);
-    }
-    $mailresult = mail($recipient, $subject, $message, $headers);
-    if ($mailresult) {
-        $json_object["status"] = "success";
-    }
-    else {
-        $json_object["status"] = "error";
-    }
-    echo json_encode(array(
-        "result" => $json_object
-    ) , JSON_PRETTY_PRINT);
-}
-else {
-    $json_object = array(
-        "status" => "error",
-        "message" => "cross origin not allowed"
-    );
-    echo json_encode(array(
-        "result" => $json_object
-    ) , JSON_PRETTY_PRINT);
-}
 ?>
